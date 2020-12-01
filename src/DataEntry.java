b4909d948a334c967d8eb2ee9299290b42f3c60b
Alexander Xu <axu@kth.se>
2020-12-01 10:37:09 +0100

    DataEntry_klar

--- src/DataEntry.java
+++ src/DataEntry.java
@@ -5,32 +5,23 @@ import java.awt.event.ActionListener;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class DataEntry extends JFrame {
+public class DataEntry {
     private Connection connection;
     private LoginForm LF;
     private ChooseProfile CP;
+    private Menu MF;
     private int chosencustomer;
-    public void setCurrentComp(JComponent e) {
-        add(e, BorderLayout.CENTER);
-        invalidate();
-        validate();
-        repaint();
-    }
+    private String chosenprofile;
 
     DataEntry() {
         connection = DataEntry.getConnection();
         LF = new LoginForm();
         CP = new ChooseProfile();
-        setTitle("Netflix :)");
-        setSize(200, 200);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-        this.add(LF);
+        MF = new Menu();
     }
 
     public static void main (String[] args) {
         new DataEntry();
-
     }
 
     public static Connection getConnection() {
@@ -56,14 +47,17 @@ public class DataEntry extends JFrame {
             return connection;
         }
     }
-    class LoginForm extends JPanel {
+    class LoginForm extends JFrame {
         private JLabel mailLabel, passLabel;
         private JTextField email_field;
         private JPasswordField password_field;
         private JButton button1;
 
-
         public LoginForm() {
+            setLayout(new FlowLayout());
+            setVisible(true);
+            setSize(Toolkit.getDefaultToolkit().getScreenSize());
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
             mailLabel = new JLabel("email");
             this.add(mailLabel);
             email_field = new JTextField(30);
@@ -74,7 +68,6 @@ public class DataEntry extends JFrame {
             this.add(password_field);
             button1 = new JButton("Login");
             this.add(button1);
-
 
             button1.addActionListener(new ActionListener() {
                 @Override
@@ -108,28 +101,28 @@ public class DataEntry extends JFrame {
         }
     }
 
-    class ChooseProfile extends JPanel {
+    class ChooseProfile extends JFrame {
         private JButton[] profile_button;
         private ArrayList<String> profile_names;
+
         public void profile_choice() {
+            LF.setVisible(false);
+            setVisible(true);
             try {
                 PreparedStatement pst = connection.prepareStatement("SELECT * FROM customerprofiles");
                 ResultSet rs = pst.executeQuery();
                 profile_names = new ArrayList<>();
+
                 while (rs.next()) {
                     if(rs.getInt(1) == chosencustomer) {
                         profile_names.add(rs.getString(3));
                     }
                 }
+
                 for (int i=0; i<profile_names.size(); i++) {
                     profile_button[i].setText(profile_names.get(i));
                     add(profile_button[i]);
                 }
-                LF.removeAll();
-                LF.validate();
-                LF.repaint();
-                setCurrentComp(this);
-
             } catch (SQLException ex) {
             }
         }
@@ -139,13 +132,48 @@ public class DataEntry extends JFrame {
             profile_button[0] = new JButton();
             profile_button[1] = new JButton();
             profile_button[2] = new JButton();
+            setSize(Toolkit.getDefaultToolkit().getScreenSize());
+            setLayout(new FlowLayout());
+            profile_button[0].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    MF.setVisible(true);
+                    MF.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+                    MF.revalidate();
+                    dispose();
+                    chosenprofile = profile_names.get(0);
+                }
+            });
+            profile_button[1].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    MF.setVisible(true);
+                    MF.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+                    MF.revalidate();
+                    dispose();
+                    chosenprofile = profile_names.get(1);
+                }
+            });
+            profile_button[2].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    MF.setVisible(true);
+                    MF.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+                    MF.revalidate();
+                    dispose();
+                    chosenprofile = profile_names.get(2);
+                }
+            });
         }
     }
 
     class Menu extends JFrame {
         private JButton searchButton;
-        private JPanel MenuPanel;
-        private JTabbedPane tabbedPane1;
+        private JPanel MenuPanel = new JPanel();
+        private JPanel pn1 = new JPanel();
+        private JPanel pn2 = new JPanel();
+        private JPanel pn3 = new JPanel();
+        private JTabbedPane tabbedPane1 = new JTabbedPane();
         private JTextField nameField;
         private JTextField yearField;
         private JTextField countryField;
@@ -163,71 +191,55 @@ public class DataEntry extends JFrame {
         private JLabel lblGenre;
         private JLabel lblRating;
 
-
         public Menu() {
-            getContentPane().add(MenuPanel);
+            tabbedPane1.add("Search",pn1);
+            tabbedPane1.add("Watchlist",pn2);
+            tabbedPane1.add("Settings",pn3);
+            add(tabbedPane1);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             pack();
-            setVisible(true);
+            setVisible(false);
 
+            /**searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            Connection connection = DataEntry.getConnection();
+            try {
+            String name = nameField.getText();
+            String year = yearField.getText();
+            String country = countryField.getText();
+            String director = directorField.getText();
+            String genre = genreField.getText();
+            String actor = actorField.getText();
+            String rating = ratingField.getText();
+            String length = lengthField.getText();
+            PreparedStatement st = connection.prepareStatement("SELECT *  FROM movies  WHERE name LIKE '%"+name+"%'");
 
-            searchButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    Connection connection = DataEntry.getConnection();
-                    try {
-                        String name = nameField.getText();
-                        String year = yearField.getText();
-                        String country = countryField.getText();
-                        String director = directorField.getText();
-                        String genre = genreField.getText();
-                        String actor = actorField.getText();
-                        String rating = ratingField.getText();
-                        String length = lengthField.getText();
-                        PreparedStatement st = connection.prepareStatement("SELECT *  FROM movies  WHERE name LIKE '%"+name+"%'");
+            ResultSet rs = st.executeQuery();
+            JFrame frame = new JFrame();
+            frame.setSize(500, 300);
+            JPanel sr = new JPanel();
+            JScrollPane srPane = new JScrollPane(sr);
 
-                        ResultSet rs = st.executeQuery();
-                        JFrame frame = new JFrame();
-                        frame.setSize(500, 300);
-                        JPanel sr = new JPanel();
-                        JScrollPane srPane = new JScrollPane(sr);
-
-                        frame.add(srPane);
-                        frame.setVisible(true);
-                        if(rs.next()) {
-                            while(rs.next()) {
-                                /**
-                                 * loggs user in and opens menu panel
-                                 */
-
-                                JLabel movielabel = new JLabel(rs.getString("name"));
-                                sr.add(movielabel);
-                                sr.revalidate();
-
-
-
-
-
-                            }
-                        }
-                        else {
-                            JOptionPane.showMessageDialog(null,"No search Result");
-                        }
-
-
-
-
-
-                    }catch(Exception ex) {
-                        ex.printStackTrace();
-                    }
-
-
-
-                }
-            });
+            frame.add(srPane);
+            frame.setVisible(true);
+            if(rs.next()) {
+            while(rs.next()) {
+            /**
+             * loggs user in and opens menu panel
+            JLabel movielabel = new JLabel(rs.getString("name"));
+            sr.add(movielabel);
+            sr.revalidate();
+            }
+            }
+            else {
+            JOptionPane.showMessageDialog(null,"No search Result");
+            }
+            }catch(Exception ex) {
+            ex.printStackTrace();
+            }
+            }
+            });*/
         }
     }
-
-
 }
